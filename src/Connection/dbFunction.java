670b72e4b85f04947516de17b5c2d7b44b531b1e
muhaiminmuhyi <muhaiminmuhyi@gmail.com>
2023-02-10 21:13:12 +0700

    update crud and search query with java

--- src/Connection/dbFunction.java
+++ src/Connection/dbFunction.java
@@ -2,6 +2,7 @@ package Connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class dbFunction {
@@ -35,11 +36,90 @@ public class dbFunction {
     public void insert_row(Connection conn, String table_name, String name_fruit, String color_fruit, int price_fruit){
         Statement statement;
         try{
-            String query = String.format("insert into %s(name_fruit, color_fruit, price_fruit) values('%s','%s',%s);", table_name, name_fruit,color_fruit,price_fruit);
+            String query = String.format("insert into kawahedu_learning.%s(name_fruit, color_fruit, price_fruit) values('%s','%s',%s);", table_name, name_fruit,color_fruit,price_fruit);
             statement = conn.createStatement();
             statement.executeUpdate(query);
             System.out.println("Data Successfully added!");
         }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void read_data(Connection conn, String table_name)
+    {
+        Statement statement;
+        ResultSet rs = null;
+        try {
+            String query = String.format("select * from kawahedu_learning.%s", table_name);
+            statement = conn.createStatement();
+            rs = statement.executeQuery(query);
+            while (rs.next()){
+                System.out.print(rs.getString("fruitid")+" ");
+                System.out.print(rs.getString("name_fruit")+" ");
+                System.out.print(rs.getString("color_fruit")+" ");
+                System.out.println(rs.getString("price_fruit")+" ");
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void update_row(Connection conn, String table_name, String new_name, String old_name){
+        Statement statement;
+        try{
+            String query = String.format("update kawahedu_learning.%s set name_fruit='%s' where name_fruit='%s';", table_name, new_name, old_name);
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Data Successfully updated!");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void searching_by_name(Connection conn, String table_name, String name_fruit)
+    {
+        Statement statement;
+        ResultSet rs = null;
+        try
+        {
+            String query = String.format("select * from kawahedu_learning.%s where name_fruit='%s';",table_name,name_fruit);
+            statement = conn.createStatement();
+            rs = statement.executeQuery(query);
+            while (rs.next()){
+                System.out.println(rs.getString("fruitid")+" ");
+                System.out.println(rs.getString("name_fruit")+" ");
+                System.out.println(rs.getString("color_fruit")+" ");
+                System.out.println(rs.getString("price_fruit")+" ");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void deleting_by_name(Connection conn, String table_name, String name_fruit)
+    {
+        Statement statement;
+        try
+        {
+            String query = String.format("delete from kawahedu_learning.%s where name_fruit ='%s'", table_name, name_fruit);
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Data has been deleted");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void delete_table(Connection conn, String table_name)
+    {
+        Statement statement;
+        try {
+            String query = String.format("delete from kawahedu_learning.%s", table_name);
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("All data was deleted!!");
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
--- src/CrudJava.java
+++ src/CrudJava.java
@@ -7,6 +7,11 @@ public class CrudJava {
         dbFunction db = new dbFunction();
         Connection conn = db.connectDb("kawah-edu", "postgres", "moemoe12.");
 //        db.createTable(conn,"tbl_fruit");
-        db.insert_row(conn, "kawahedu_learning.tbl_fruit", "Anggur", "Ungu", 5000);
+//        db.insert_row(conn, "tbl_fruit", "Anggur", "Ungu", 5000);
+//        db.read_data(conn,"tbl_fruit");
+//        db.update_row(conn, "tbl_fruit", "Mengkudu","Anggur");
+//        db.searching_by_name(conn,"tbl_fruit","Mengkudu");
+//        db.deleting_by_name(conn, "tbl_fruit","orange");
+//        db.delete_table(conn, "tbl_fruit");
     }
 }
